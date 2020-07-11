0818adc0f64b9effb827d95229010ddbe52a50c3
LauriH-A <49798989+LauriH-A@users.noreply.github.com>
2020-07-11 18:05:12 -0300

    Actividadesv1

---
+++ GimnasioB/src/controladores/SistemaActividades.java
@@ -0,0 +1,102 @@
+package controladores;
+
+import java.util.Vector;
+import modelo.Actividad;
+import persistencia.ActividadesMapper;
+
+public class SistemaActividades {
+	
+	private  Vector<Actividad>vActividades;
+	private  static SistemaActividades instanciaSistemaActividades;
+	
+	//Constructor
+	public SistemaActividades() {
+		
+		vActividades=new Vector<Actividad>();
+				
+	}
+	
+	//Singleton
+	
+	public static SistemaActividades getInstancia(){
+		if(instanciaSistemaActividades==null){
+			instanciaSistemaActividades=new SistemaActividades();
+		}
+		
+		return instanciaSistemaActividades;
+	}
+	
+	//Nueva Actividad
+	public void crearActividad(String nombreActividad,String estadoActividad)
+	{
+		if(!existeActividad(nombreActividad)) {
+	
+			Actividad a = new Actividad(nombreActividad,estadoActividad);
+			vActividades.add(a);
+			a.esActivo(); //Agrega una nueva actividad con estado inicial "Activo"
+			ActividadesMapper.getInstance().altaActividad(a);
+		}
+	}
+	
+	//Eliminar Actividad fisicamente
+	public int bajarActividad (Actividad act, int flag) {
+		
+		Actividad a= buscarActividad(act.getNombreActividad());
+				
+		if(a!=null) {
+			vActividades.remove(a);
+			a.bajaLogica();
+			ActividadesMapper.getInstance().updateActividad(a);
+			return 1;
+		}
+		return flag;
+	}
+	
+	//Modificar
+	public void modificarActividad(String nombreActividad, String estado){
+		
+		for(Actividad a: vActividades) {
+			if(a.getNombreActividad().toString().equals(nombreActividad) && a.getEstado().toString().contentEquals(estado))
+				a.setNombreActividad(nombreActividad);//solo actualiza el nombre de la actividad
+			
+			a.bajaLogica(); //Solo acutaliza el estado de la actividad
+			ActividadesMapper.getInstance().updateActividad(a);
+		}
+	
+	}
+	
+	
+	public boolean existeActividad (String nombreActividad) {
+
+		Actividad a = buscarActividad(nombreActividad);
+		if(a!=null)
+			return true;
+
+		return false;		
+	}
+	
+	
+	public Actividad buscarActividad(String nombreActividad) {
+		
+		/*----Buscamos en memoria----*/
+		for(Actividad a: vActividades)
+		{
+			if(a.getNombreActividad().equals(nombreActividad)) {
+					return a;	
+			}
+		}
+		/*----Si no esta, buscamos en BD----*/
+		
+		Actividad a = ActividadesMapper.getInstance().buscarActividad(nombreActividad);
+		 
+		/*----Agrega Actividad PARA FUTURAS BUSQUEDAS----*/
+		
+		if(a!=null)
+			vActividades.add(a);
+			
+		return a;
+	}
+	
+	
+
+}
---
+++ GimnasioB/src/persistencia/ActividadesMapper.java
@@ -0,0 +1,114 @@
+package persistencia;
+import java.util.Vector;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import modelo.Actividad;
+
+
+public class ActividadesMapper {
+	private static ActividadesMapper instancia;
+	private static Vector <Actividad> vActividades;
+	/*----CONSTRUCTOR 1----*/
+	
+	private ActividadesMapper()
+	{
+		
+	}
+	
+	/*----SINGLETON----*/
+	
+	public static ActividadesMapper getInstance()
+	{
+		if (instancia == null)
+			instancia = new ActividadesMapper();
+		return instancia;
+	}
+	
+	//Alta Actividad
+	public void altaActividad(Actividad A)
+	{	/*----TRY DE LA CONECCION INSERT----*/
+		try
+		{
+			Actividad act = (Actividad) A;
+			Connection con = PoolConnection.getPoolConnection().getConnection();
+			/*----STATEMENT QUERY DEL INSERT----*/
+				PreparedStatement s = con.prepareStatement("insert into dbo.Actividades(actividad, estadoActividad) values (?,?)");
+				/*-----CAMPOS DEL Actividad----*/
+				s.setString(1, act.getNombreActividad());
+				s.setString(2, act.getEstado());
+				s.execute();
+				PoolConnection.getPoolConnection().realeaseConnection(con);
+		}
+		catch (Exception e)
+		{
+			System.out.println("rompio insert actividad");
+			System.out.println("Stack Trace: " + e.getStackTrace() + e.getMessage());
+		}
+	}
+
+	public Actividad buscarActividad (String actividad) {
+		try {
+			Actividad act= null;
+			String nombreAct =actividad;
+			
+			Connection con = PoolConnection.getPoolConnection().getConnection();
+			PreparedStatement s = con.prepareStatement("select * from dbo.Actividades where actividad = ?");
+			s.setString(1, nombreAct);
+			ResultSet result = s.executeQuery();
+			while(result.next()) {
+				nombreAct = result.getString(1);
+			}
+			PoolConnection.getPoolConnection().realeaseConnection(con);
+			return act;
+		}
+		catch (Exception e) {
+			System.out.println("rompio select Act");
+			System.out.println("Stack Trace: " + e.getStackTrace() + e.getMessage());
+			}
+		return null;
+	}
+	
+	public ResultSet listar() {
+		try {
+			Actividad a =null;
+			Connection con = PoolConnection.getPoolConnection().getConnection();
+			PreparedStatement s = con.prepareStatement("select * from dbo.Actividades where estadoActividad = 'Activo'");
+			ResultSet result = s.executeQuery();
+			PoolConnection.getPoolConnection().realeaseConnection(con);
+			return result;
+		}
+		catch (Exception e) {
+			System.out.println("Error select listar Actividades\n");
+			System.out.println("Stack Trace: " + e.getStackTrace() + e.getMessage());
+		}
+		return null;
+	}
+	
+	public void updateActividad (Actividad actividad) {
+		try {
+			Actividad a = (Actividad) actividad;
+			Connection con = PoolConnection.getPoolConnection().getConnection();
+			/*----STATEMENT QUERY DEL UPDATE----*/
+			PreparedStatement s = con.prepareStatement("update dbo.Actividades " +
+					"set actividad =?," +
+					" estadoActividad =?" + " where actividad = ?");
+			
+			s.setString(1, a.getNombreActividad());
+			s.setString(2, a.getEstado());
+			
+			//filtro del Where
+			s.setString(3, a.getNombreActividad());
+			s.execute();
+			PoolConnection.getPoolConnection().realeaseConnection(con);
+		}
+		catch (Exception e)
+		{
+			System.out.println("rompio update actividad");
+			System.out.println("Stack Trace: " + e.getStackTrace() + e.getMessage());
+		}
+	}
+	
+	
+}
